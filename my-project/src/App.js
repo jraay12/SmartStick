3e1062ad8a15cea5790489cf85bf75d4ae425b53
John Ray <johnraycaete@gmail.com>
2023-10-25 00:23:50 +0800

    Add routes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 
+
+
 function App() {
-  return <Login/>
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/Login" />} />
+      <Route path="/Login" element={<Login />} />
+    </Routes>
+  )
 }
 
 export default App;
